4986666fa846c7743d1d0f140543eef2d1b2300a
raxracks <multisniperism@gmail.com>
2022-06-05 03:15:18 +1200

    Add project files.

---
+++ Xngn/XngnMain.cpp
@@ -0,0 +1,140 @@
+﻿#include "pch.h"
+#include "XngnMain.h"
+#include "Common\DirectXHelper.h"
+#include "imgui/imgui.h"
+#include "imgui_impl_dx11.h"
+
+using namespace Xngn;
+using namespace Windows::Foundation;
+using namespace Windows::System::Threading;
+using namespace Concurrency;
+
+// Loads and initializes application assets when the application is loaded.
+XngnMain::XngnMain(const std::shared_ptr<DX::DeviceResources>& deviceResources) :
+	m_deviceResources(deviceResources)
+{
+	// Register to be notified if the Device is lost or recreated
+	m_deviceResources->RegisterDeviceNotify(this);
+
+	// TODO: Replace this with your app's content initialization.
+	m_sceneRenderer = std::unique_ptr<Sample3DSceneRenderer>(new Sample3DSceneRenderer(m_deviceResources));
+
+	m_fpsTextRenderer = std::unique_ptr<SampleFpsTextRenderer>(new SampleFpsTextRenderer(m_deviceResources));
+
+	m_timer.SetFixedTimeStep(true);
+	m_timer.SetTargetElapsedSeconds(1.0 / 60);
+
+	// Setup Dear ImGui context
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO& io = ImGui::GetIO();
+
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+
+	// Setup Dear ImGui style
+	ImGui::StyleColorsDark();
+	//ImGui::StyleColorsClassic();
+
+	ImGui_ImplDX11_Init(m_deviceResources->GetD3DDevice(), m_deviceResources->GetD3DDeviceContext());
+}
+
+XngnMain::~XngnMain()
+{
+	// Deregister device notification
+	m_deviceResources->RegisterDeviceNotify(nullptr);
+}
+
+// Updates application state when the window size changes (e.g. device orientation change)
+void XngnMain::CreateWindowSizeDependentResources()
+{
+	// TODO: Replace this with the size-dependent initialization of your app's content.
+	m_sceneRenderer->CreateWindowSizeDependentResources();
+}
+
+// Updates the application state once per frame.
+void XngnMain::Update()
+{
+	// Update scene objects.
+	m_timer.Tick([&]()
+		{
+			// TODO: Replace this with your app's content update functions.
+			m_sceneRenderer->Update(m_timer);
+			m_fpsTextRenderer->Update(m_timer);
+		});
+}
+
+// Renders the current frame according to the current application state.
+// Returns true if the frame was rendered and is ready to be displayed.
+bool XngnMain::Render()
+{
+	ImGuiIO& io = ImGui::GetIO();
+
+	// Don't try to render anything before the first Update.
+	if (m_timer.GetFrameCount() == 0)
+	{
+		return false;
+	}
+
+	auto context = m_deviceResources->GetD3DDeviceContext();
+
+	// Reset the viewport to target the whole screen.
+	auto viewport = m_deviceResources->GetScreenViewport();
+	context->RSSetViewports(1, &viewport);
+
+	io.DisplaySize = ImVec2(viewport.Width, viewport.Height);
+
+	// Reset render targets to the screen.
+	ID3D11RenderTargetView* const targets[1] = { m_deviceResources->GetBackBufferRenderTargetView() };
+	context->OMSetRenderTargets(1, targets, m_deviceResources->GetDepthStencilView());
+
+	// Clear the back buffer and depth stencil view.
+	context->ClearRenderTargetView(m_deviceResources->GetBackBufferRenderTargetView(), DirectX::Colors::CornflowerBlue);
+	context->ClearDepthStencilView(m_deviceResources->GetDepthStencilView(), D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0);
+
+	m_sceneRenderer->Render();
+	m_fpsTextRenderer->Render();
+
+	// Start the Dear ImGui frame
+	ImGui_ImplDX11_NewFrame();
+	ImGui::NewFrame();
+
+	// 2. Show a simple window that we create ourselves. We use a Begin/End pair to created a named window.
+	{
+		static float f = 0.0f;
+		static int counter = 0;
+
+		ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+		ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+		if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+			counter++;
+		ImGui::SameLine();
+		ImGui::Text("counter = %d", counter);
+
+		ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+		ImGui::End();
+	}
+
+	// Rendering
+	ImGui::Render();
+	ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
+
+	return true;
+}
+
+// Notifies renderers that device resources need to be released.
+void XngnMain::OnDeviceLost()
+{
+	m_sceneRenderer->ReleaseDeviceDependentResources();
+	m_fpsTextRenderer->ReleaseDeviceDependentResources();
+}
+
+// Notifies renderers that device resources may now be recreated.
+void XngnMain::OnDeviceRestored()
+{
+	m_sceneRenderer->CreateDeviceDependentResources();
+	m_fpsTextRenderer->CreateDeviceDependentResources();
+	CreateWindowSizeDependentResources();
+}
